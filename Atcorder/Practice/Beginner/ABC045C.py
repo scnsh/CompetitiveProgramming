331cdb9145a027c627dbd159576dba5551a164d6
Naoki Numaguchi <naoki.numaguchi@groove-x.com>
2019-08-12 09:44:23 +0900

    add abc045c

--- Atcorder/Practice/Beginner/ABC045C.py
+++ Atcorder/Practice/Beginner/ABC045C.py
@@ -1,4 +1,14 @@
-S = list(map(int, input().split()))
+S = input()
 len_s = len(S)-1
+count = 0
+ss = []
 for i in range(2**len_s):
-  
+  binary = format(i, 'b').zfill(len_s+2)[2:]
+  tmp=[S[0]]
+  for j in range(len(binary)):
+    if binary[j] =='1':
+      tmp.append(S[j+1])
+    else:
+      tmp[-1] = tmp[-1]+S[j+1]
+  count += sum(int(i) for i in tmp)
+print(count)
